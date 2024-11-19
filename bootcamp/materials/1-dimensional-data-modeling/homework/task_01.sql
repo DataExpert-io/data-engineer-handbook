3742e8c23089151c2dd743b33f84f795a2ced78b
Fernando Stahelin <fernandostahelin@gmail.com>
2024-11-19 11:35:55 -0300

    task 01

---
+++ bootcamp/materials/1-dimensional-data-modeling/homework/task_01.sql
@@ -0,0 +1,8 @@
+CREATE TABLE actors (
+            actor text,
+            actorid text,
+            current_year Integer
+            films films,
+            quality_class quality_class,
+            is_active BOOLEAN
+)
