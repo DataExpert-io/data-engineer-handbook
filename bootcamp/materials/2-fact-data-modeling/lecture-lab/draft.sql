e7ba4c035a4b44f818612486badd245eba163683
Lujein <lujein.alsheikh@gmail.com>
2024-12-02 17:26:58 +0100

    lab3 code

---
+++ bootcamp/materials/2-fact-data-modeling/lecture-lab/draft.sql
@@ -0,0 +1,23 @@
+-- SELECT ARRAY(
+--         SELECT 0::REAL
+--         FROM generate_series(1, ('2023-01-02'::DATE - '2023-01-01'::DATE))
+--     );
+-- drop table array_metrics;
+-- CREATE TABLE array_metrics (
+--     user_id NUMERIC,
+--     month_start DATE,
+--     metric_name TEXT,
+--     metric_array REAL [],
+--     PRIMARY KEY(user_id, month_start, metric_name)
+-- )
+-- SELECT DATE(event_time),
+--     count(*)
+-- from events
+-- where user_id = '10060569187331700000'
+-- GROUP BY DATE(event_time);
+SELECT *
+from array_metrics;
+SELECT cardinality(metric_array),
+    count(1)
+from array_metrics
+GROUP BY 1;
