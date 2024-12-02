a73bb2e2c726746001c4429f43ab3e337322cfdc
zerangmajid <zerangmajid@gmail.com>
2024-12-02 15:10:58 +0330

    Update 2_insert_historical_data.sql

--- bootcamp/materials/1-dimensional-data-modeling/zerangmajid/ Incremental/2_insert_historical_data.sql
+++ bootcamp/materials/1-dimensional-data-modeling/zerangmajid/ Incremental/2_insert_historical_data.sql
@@ -1,13 +1,13 @@
 -- Insert historical data into players_scd
 INSERT INTO players_scd (player_name, scoring_class, is_active, start_season, end_season, current_season)
 VALUES
-    ('Player A', 'Elite', TRUE, 2020, 2021, 2021),
-    ('Player B', 'Average', FALSE, 2019, 2021, 2021),
-    ('Player C', 'Elite', TRUE, 2020, 2021, 2021);
+    ('Player A', 'Good', TRUE, 2020, 2021, 2021),
+    ('Player B', 'Bad', FALSE, 2019, 2021, 2021),
+    ('Player C', 'Good', TRUE, 2020, 2021, 2021);
 
 -- Insert current season data into players
 INSERT INTO players (player_name, scoring_class, is_active, current_season)
 VALUES
-    ('Player A', 'Elite', TRUE, 2022),
-    ('Player B', 'Elite', TRUE, 2022), -- Changed scoring class
-    ('Player D', 'Rookie', TRUE, 2022); -- New player
+    ('Player A', 'Good', TRUE, 2022),
+    ('Player B', 'Good', TRUE, 2022), -- Changed scoring class
+    ('Player D', 'Bad', TRUE, 2022); -- New player
