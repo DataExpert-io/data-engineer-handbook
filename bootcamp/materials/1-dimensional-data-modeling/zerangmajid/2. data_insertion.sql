386f7e94a9c0f97868cac06151a24a2114be0674
zerangmajid <zerangmajid@gmail.com>
2024-12-02 09:28:33 +0330

    Update and rename data_insertion.sql to 2. data_insertion.sql

---
+++ bootcamp/materials/1-dimensional-data-modeling/zerangmajid/2. data_insertion.sql
@@ -0,0 +1,29 @@
+#This file contains SQL for inserting data into the tables:
+INSERT INTO vertices
+SELECT
+    team_id AS identifier,
+    'team'::vertex_type AS type,
+    json_build_object(
+        'abbreviation', abbreviation,
+        'nickname', nickname,
+        'city', city,
+        'arena', arena,
+        'year_founded', yearfounded
+    ) AS properties
+FROM teams;
+
+INSERT INTO edges
+SELECT
+    player_id AS subject_identifier,
+    'player'::vertex_type AS subject_type,
+    game_id AS object_identifier,
+    'game'::vertex_type AS object_type,
+    'plays_in'::edge_type AS edge_type,
+    json_build_object(
+        'start_position', start_position,
+        'pts', pts,
+        'team_id', team_id,
+        'team_abbreviation', team_abbreviation
+    ) AS properties
+FROM game_details;
+
--- bootcamp/materials/1-dimensional-data-modeling/zerangmajid/data_insertion.sql
+++
@@ -1,1 +0,0 @@
-# SQL scripts for inserting data
