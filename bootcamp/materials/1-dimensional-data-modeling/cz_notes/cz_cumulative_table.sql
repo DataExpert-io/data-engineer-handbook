025698c6fe9724f53f69c6e0db77eb1a29cf9417
Christine Zhang <zhangwen08@gmail.com>
2024-12-09 22:20:49 +1100

    homework

--- bootcamp/materials/1-dimensional-data-modeling/cz_notes/cz_cumulative_table.sql
+++ bootcamp/materials/1-dimensional-data-modeling/cz_notes/cz_cumulative_table.sql
@@ -1,27 +1,27 @@
 -- select *
 --  from player_seasons
  
---  create type season_stats as (
---  	season integer,
--- 	 gp integer,
--- 	 pts real,
--- 	 reb real,
--- 	 ast real
---  )
+ create type season_stats as (
+ 	season integer,
+	 gp integer,
+	 pts real,
+	 reb real,
+	 ast real
+ )
  
  
---  create table players (
---  	player_name text,
--- 	 height  text,
--- 	 college text,
--- 	 country text, 
--- 	 draft_year text,
--- 	 draft_round text,
--- 	 draft_number text,
--- 	 season_stats season_stats[],
--- 	 current_season integer,
--- 	 primary key (player_name, current_season)
---  )
+ create table players (
+ 	player_name text,
+	 height  text,
+	 college text,
+	 country text, 
+	 draft_year text,
+	 draft_round text,
+	 draft_number text,
+	 season_stats season_stats[],
+	 current_season integer,
+	 primary key (player_name, current_season)
+ )
 
 
 /*
@@ -29,36 +29,37 @@
 	today: holding schema of the source table that will be added into the cumulated table.
 */
 
--- insert into players
--- with yesterday as (
--- 	select * from players 
--- 	where current_season = 2000
--- ),
--- today as (
--- 	select * from player_seasons
--- 	where season = 2001
--- )
+insert into players
+with yesterday as (
+	select * from players 
+	where current_season = 2000
+),
+today as (
+	select * from player_seasons
+	where season = 2001
+)
 
--- select
--- 	coalesce(t.player_name, y.player_name) as player_name,
--- 	coalesce(t.height, y.height) as height,
--- 	coalesce(t.college, y.college) as college,
--- 	coalesce(t.country, y.country) as country,
--- 	coalesce(t.draft_year, y.draft_year) as draft_year,
--- 	coalesce(t.draft_round, y.draft_round) as draft_round,
--- 	coalesce(t.draft_number, y.draft_number) as draft_number,
--- 	case 
--- 	-- if yesterday table has no season_stats data
--- 		when y.season_stats is null then array[row(t.season,t.gp,t.pts,t.reb,t.ast)::season_stats]
--- 		-- if yesterday table has data, and also today table has season_stats data
--- 		when t.season is not null then y.season_stats || array[row(t.season,t.gp,t.pts,t.reb,t.ast)::season_stats]
--- 		else y.season_stats
--- 	end as season_stats,
--- 	coalesce(t.season,y.current_season + 1) as current_season
+select
+	coalesce(t.player_name, y.player_name) as player_name,
+	coalesce(t.height, y.height) as height,
+	coalesce(t.college, y.college) as college,
+	coalesce(t.country, y.country) as country,
+	coalesce(t.draft_year, y.draft_year) as draft_year,
+	coalesce(t.draft_round, y.draft_round) as draft_round,
+	coalesce(t.draft_number, y.draft_number) as draft_number,
+	case 
+	-- if yesterday table has no season_stats data
+		when y.season_stats is null then array[row(t.season,t.gp,t.pts,t.reb,t.ast)::season_stats]
+		-- if yesterday table has data, and also today table has season_stats data
+		when t.season is not null then y.season_stats || array[row(t.season,t.gp,t.pts,t.reb,t.ast)::season_stats]
+		-- if today has no data
+		else y.season_stats
+	end as season_stats,
+	coalesce(t.season,y.current_season + 1) as current_season
 	
 
--- from today t Full Outer Join yesterday y
--- on t.player_name = y.player_name 
+from today t Full Outer Join yesterday y
+on t.player_name = y.player_name 
 
 /*
 	unnest array of structs column
