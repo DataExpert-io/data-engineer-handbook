d9bec907537d75640ae8919552a4617fbf7de925
Helen Cristina <helensilva14@gmail.com>
2025-07-10 19:08:45 -0400

    [MOD1] Add script for Task 3

---
+++ bootcamp/materials/1-dimensional-data-modeling/homework/ddl_actors_history_scd.sql
@@ -0,0 +1,9 @@
+CREATE TABLE actors_history_scd (
+    actor_id TEXT,
+    actor_name TEXT,
+    quality_class quality_class,
+    is_active BOOLEAN,
+    current_year INTEGER,
+    start_date INTEGER,
+    end_date INTEGER
+);
