f6d94f3016a3976af7bf8e4d156edbd144a2413c
Helen Cristina <helensilva14@gmail.com>
2025-07-11 17:09:25 -0400

    [MOD1] Scripts for Tasks 3 and 4

--- bootcamp/materials/1-dimensional-data-modeling/homework/actors_scd_backfill_query.sql
+++ bootcamp/materials/1-dimensional-data-modeling/homework/actors_scd_backfill_query.sql
@@ -1,11 +1,50 @@
-WITH previous_values AS (
+WITH change_indicators AS (
     SELECT
         actor_id, 
         actor_name, 
         quality_class, 
         is_active,
         current_year,
-        LAG(quality_class, 1) OVER 
-            (PARTITION BY actor_id ORDER BY current_year)
+        LAG(quality_class, 1) OVER (PARTITION BY actor_id ORDER BY current_year) 
+            <> quality_class AS quality_class_changed,
+        LAG(is_active, 1) OVER (PARTITION BY actor_id ORDER BY current_year) 
+            <> is_active AS is_active_changed
     FROM actors
 ),
+streak_identified AS (
+    SELECT
+        actor_id, 
+        actor_name, 
+        quality_class, 
+        is_active,
+        current_year,
+        SUM(CASE 
+                WHEN quality_class_changed THEN 1 
+                WHEN is_active_changed THEN 1 
+                ELSE 0
+            END) OVER (PARTITION BY actor_id ORDER BY current_year) 
+            AS streak_identifier
+    FROM change_indicators
+),
+aggregated AS (
+    SELECT
+        actor_id, 
+        actor_name, 
+        quality_class, 
+        is_active,
+        streak_identifier,
+        MIN(current_year) AS start_date,
+        MAX(current_year) AS end_date
+    FROM streak_identified
+    GROUP BY actor_id, actor_name, quality_class, is_active, streak_identifier
+)
+INSERT INTO actors_history_scd
+SELECT 
+    actor_id, 
+    actor_name, 
+    quality_class, 
+    is_active, 
+    start_date, 
+    end_date
+FROM aggregated
+ORDER BY actor_id, actor_name, start_date;
--- bootcamp/materials/1-dimensional-data-modeling/homework/ddl_actors_history_scd.sql
+++ bootcamp/materials/1-dimensional-data-modeling/homework/ddl_actors_history_scd.sql
@@ -3,7 +3,7 @@ CREATE TABLE actors_history_scd (
     actor_name TEXT,
     quality_class quality_class,
     is_active BOOLEAN,
-    current_year INTEGER,
     start_date INTEGER,
-    end_date INTEGER
+    end_date INTEGER,
+    PRIMARY KEY (actor_id, start_date)
 );
