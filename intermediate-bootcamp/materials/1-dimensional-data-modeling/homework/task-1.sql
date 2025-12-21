84af3515870db444fc0ffe63dfc8995a994d3338
Ramandeep Singh <ramanvirdiz@gmail.com>
2025-12-22 02:01:57 +0530

    fix: week1 - task1 assignment

--- intermediate-bootcamp/materials/1-dimensional-data-modeling/homework/task-1.sql
+++ intermediate-bootcamp/materials/1-dimensional-data-modeling/homework/task-1.sql
@@ -16,45 +16,26 @@ quality_class: This field represents an actor's performance quality, determined 
 is_active: A BOOLEAN field that indicates whether an actor is currently active in the film industry (i.e., making films this year).
 */
 
-SELECT *
-FROM actor_films;
-
-create table public.actor_films
-(
-    actor   text,
-    actorid text not null,
-    film    text,
-    year    integer,
-    votes   integer,
-    rating  real,
-    filmid  text not null,
-    primary key (actorid, filmid)
-);
-
-alter table public.actor_films
-    owner to postgres
-
-
-
 -- films struct
 CREATE TYPE films AS
 (
-    film,
-    votes,
-    rating,
-    filmid
+    film   TEXT,
+    votes  INTEGER,
+    rating REAL,
+    filmid TEXT
 );
 
 -- quality_class struct of type ENUM
 CREATE TYPE quality_class AS ENUM ('star', 'good', 'average', 'bad');
 
 -- DDL for an actors table
-CREATE TABLE actors (
-    actor text,
-    actor_id text NOT NULL,
-    films films[],
+CREATE TABLE actors
+(
+    actorid       TEXT,
+    current_year  INTEGER,
+    actor         TEXT,
+    films         films[],
     quality_class quality_class,
-    is_active BOOLEAN
-
-)
-
+    is_active     BOOLEAN,
+    PRIMARY KEY (actorid, current_year)
+);
