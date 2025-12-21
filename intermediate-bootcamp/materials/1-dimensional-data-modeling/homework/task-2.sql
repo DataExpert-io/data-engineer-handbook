38033fd240ea0772d35daf986acb30b833a6e64b
Ramandeep Singh <ramanvirdiz@gmail.com>
2025-12-22 02:02:09 +0530

    feat: week1 - task2 assignment

---
+++ intermediate-bootcamp/materials/1-dimensional-data-modeling/homework/task-2.sql
@@ -0,0 +1,52 @@
+-- Cumulative table generation query: Write a query that populates the actors table one year at a time.
+
+DO
+$$
+    DECLARE
+        y_year INT;
+        t_year INT;
+    BEGIN
+        FOR y_year IN 1969..2020
+            LOOP
+                t_year := y_year + 1;
+                INSERT INTO actors
+                WITH yesterday AS (SELECT *
+                                   FROM actors
+                                   WHERE current_year = y_year),
+                     today_aggregated AS (SELECT actorid,
+                                                 MAX(year)                                           AS year,
+                                                 MAX(actor)                                          AS actor,
+                                                 ARRAY_AGG(ROW (film, votes, rating, filmid)::films) AS films,
+                                                 AVG(rating)                                         AS average_rating
+                                          FROM actor_films
+                                          WHERE year = t_year
+                                          GROUP BY actorid)
+
+                SELECT COALESCE(t.actorid, y.actorid)       AS actorid,
+                       COALESCE(t.year, y.current_year + 1) AS current_year,
+                       COALESCE(t.actor, y.actor)           AS actor,
+                       CASE
+                           WHEN y.films IS NULL THEN t.films
+                           WHEN t.films IS NOT NULL THEN y.films || t.films
+                           ELSE y.films
+                           END                              AS films,
+                       CASE
+                           WHEN t.films IS NOT NULL THEN
+                               (CASE
+                                    WHEN t.average_rating > 8 THEN 'star'
+                                    WHEN t.average_rating > 7 THEN 'good'
+                                    WHEN t.average_rating > 6 THEN 'average'
+                                    ELSE 'bad'
+                                   END)::quality_class
+                           ELSE y.quality_class
+                           END                              AS quality_class,
+                       CASE
+                           WHEN t.films IS NOT NULL THEN TRUE
+                           ELSE FALSE
+                           END                              AS is_active
+                FROM today_aggregated t
+                         FULL OUTER JOIN yesterday y ON t.actorid = y.actorid;
+
+            END LOOP;
+    END
+$$;
