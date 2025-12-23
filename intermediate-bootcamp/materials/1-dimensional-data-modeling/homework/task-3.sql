27b032c15a22ba3bd090201e3eeb9c64b27dd58b
Ramandeep Singh <ramanvirdiz@gmail.com>
2025-12-24 01:41:48 +0530

    feat: create DDL for actors_history_scd table with type 2 dimension modeling

--- intermediate-bootcamp/materials/1-dimensional-data-modeling/homework/task-3.sql
+++ intermediate-bootcamp/materials/1-dimensional-data-modeling/homework/task-3.sql
@@ -1,19 +1,23 @@
-/*
-DDL for actors_history_scd table: Create a DDL for an actors_history_scd table with the following features:
+-- Task 3: DDL for actors_history_scd table
+-- Create a DDL for an actors_history_scd table with the following features:
+--   - Implements type 2 dimension modeling (includes start_date and end_date fields)
+--   - Tracks quality_class and is_active status for each actor in the actors table
 
-- Implements type 2 dimension modeling (i.e., includes start_date and end_date fields).
-- Tracks quality_class and is_active status for each actor in the actors table.
-*/
+CREATE TYPE actors_scd_type AS
+(
+    quality_class actor_quality_class,
+    is_active     BOOLEAN,
+    start_date    INTEGER,
+    end_date      INTEGER
+);
 
-
--- DDL for actors_history_scd table
 CREATE TABLE actors_history_scd
 (
     actorid       TEXT,
-    quality_class quality_class,
+    quality_class actor_quality_class,
     is_active     BOOLEAN,
     start_date    INTEGER,
     end_date      INTEGER,
     current_year  INTEGER,
-    PRIMARY KEY (actorid, start_date)
+    PRIMARY KEY (actorid, start_date, current_year)
 );
